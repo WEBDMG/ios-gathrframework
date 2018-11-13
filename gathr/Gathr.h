291e5e7a20282489abd84557780682fe48a0b08d
Richard Robinson <kincade71@gmail.com>
2018-11-13 16:47:01 -0500

    Updated ref to gathr with capital Gathr

--- gathr/Gathr.h
+++ gathr/Gathr.h
@@ -9,10 +9,10 @@
 #import <UIKit/UIKit.h>
 
 //! Project version number for gathr.
-FOUNDATION_EXPORT double gathrVersionNumber;
+FOUNDATION_EXPORT double GathrVersionNumber;
 
 //! Project version string for gathr.
-FOUNDATION_EXPORT const unsigned char gathrVersionString[];
+FOUNDATION_EXPORT const unsigned char GathrVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <gathr/PublicHeader.h>
 
